5c7ef93962fb0a70dda73a0ef0f9e586c31e9877
Anderson Witt <anderson.witt@worktrucksolutions.com>
2024-04-27 15:53:22 -0300

    wip

--- app.js
+++ app.js
@@ -1,18 +1,9 @@
 import express from "express";
 import indexRouter from "./routes/route.js";
 import bodyParser from "body-parser";
-import session from "express-session";
 
 const PORT = process.env.PORT || 8000;
 const app = express();
-
-app.use(
-  session({
-    secret: "12345",
-    resave: false,
-    saveUninitialized: true,
-  })
-);
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -24,6 +15,9 @@ app.use(express.static("public"));
 
 app.use("/", indexRouter);
 
+
 app.listen(PORT, () => {
-  console.log(`Servidor iniciado na porta ${PORT} no endereço http://localhost:${PORT}`);
+  console.log(
+    `Servidor iniciado na porta ${PORT} no endereço http://localhost:${PORT}`
+  );
 });
--- middlewares/initialAuthMiddleware.js
+++ middlewares/initialAuthMiddleware.js
@@ -1,9 +1,25 @@
+import jwt from "jsonwebtoken";
+import mapCookies from "../utils/cookie.js";
+const secretKey = "your_secret_key";
+
 const initialRouteMiddleware = (req, res, next) => {
-  if (!req.session.isAuthenticated) {
+  const cookieString = req.headers["cookie"];
+  const cookies = mapCookies(cookieString);
+  
+
+  if (!cookies?.auth) {
     next();
-  } else {
-    res.redirect("/home");
+    return;
   }
+
+  jwt.verify(cookies.auth, secretKey, (err, user) => {
+    if (err) {
+      next();
+    } else {
+      req.user = user;
+      res.redirect("/home");
+    }
+  });
 };
 
 export default initialRouteMiddleware;
